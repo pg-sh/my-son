89faf466e3e86720534a5b082075cb0add9be14b
pg-sh <tjdgus4722@naver.com>
2019-02-03 04:11:44 +0900

    initial commit

---
+++ src/main/java/com/son/MySonApplication.java
@@ -0,0 +1,14 @@
+package com.son;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySonApplication.class, args);
+	}
+
+}
+
