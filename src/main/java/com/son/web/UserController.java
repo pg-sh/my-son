8d3ee75d393bc6fac04bef06db6a06be5dd6e00f
pg-sh <tjdgus4722@naver.com>
2019-02-10 04:41:11 +0900

    no message

--- src/main/java/com/son/web/UserController.java
+++ src/main/java/com/son/web/UserController.java
@@ -21,26 +21,26 @@ public class UserController {
 	
 	@GetMapping("/form")
 	public String form() {
-		return "/user/form";
+		return "user/form";
 	}
 	
 	@PostMapping("")
 	public String create(User user) {
 		userRepository.save(user);
-		return "redirect:/users";
+		return "redirect:users";
 	}
 	
 	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "/user/list";
+		return "user/list";
 	}
 	
 	@GetMapping("{id}/form")
 	public String updateForm(@PathVariable Long id, Model model) {
 		User user = userRepository.findById(id).get();
 		model.addAttribute("user", user);
-		return "/user/updateForm";
+		return "user/updateForm";
 	}
 	
 	@PutMapping("/{id}")
@@ -48,6 +48,6 @@ public class UserController {
 		User user = userRepository.findById(id).get();
 		user.update(newUser);
 		userRepository.save(user);
-		return "redirect:/users";
+		return "redirect:users";
 	}
 }
