c094edff8cbb79b027bd1a7c5359d79186863c93
pg-sh <tjdgus4722@naver.com>
2019-02-18 16:07:19 +0900

    답변 추가 및 답변 목록 기능 구현

---
+++ src/main/java/com/son/web/AnswerController.java
@@ -0,0 +1,37 @@
+package com.son.web;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.son.domain.Answer;
+import com.son.domain.AnswerRepository;
+import com.son.domain.Question;
+import com.son.domain.QuestionRepository;
+import com.son.domain.User;
+
+@Controller
+@RequestMapping("/question/{questionId}/answers")
+public class AnswerController {
+	@Autowired
+	private AnswerRepository answerRepository;
+	
+	@Autowired
+	private QuestionRepository questionRepository;
+	
+	@PostMapping("") 
+	public String create(@PathVariable Long questionId, String contents, HttpSession session) {
+		if(!HttpSessionUtils.isLoginUser(session)) {
+			return "users/loginForm";
+		}
+		User loginUser = HttpSessionUtils.getUserFromSession(session);
+		Question question = questionRepository.findById(questionId).get();
+		Answer answer = new Answer(loginUser, question, contents);
+		answerRepository.save(answer);
+		return String.format("redirect:/questions/%d", questionId);
+	}
+}
